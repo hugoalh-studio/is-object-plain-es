1f1edd8abc53019d2cb6a19389da480b96b84703
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-03-27 12:04:47 +0800

    Simplify detection

--- mod.ts
+++ mod.ts
@@ -25,59 +25,26 @@ export function isObjectPlain(item: object): boolean {
 	if (Object.getOwnPropertySymbols(item).length > 0) {
 		return false;
 	}
-	const itemEntriesConfigurable: string[] = [];
-	const itemEntriesEnumerable: string[] = [];
-	const itemEntriesGetter: string[] = [];
-	const itemEntriesNonAccessor: string[] = [];
-	const itemEntriesNonConfigurable: string[] = [];
-	const itemEntriesNonEnumerable: string[] = [];
-	const itemEntriesNonWritable: string[] = [];
-	const itemEntriesSetter: string[] = [];
-	const itemEntriesWritable: string[] = [];
 	const itemDescriptors = Object.getOwnPropertyDescriptors(item);
+	let enumerableCount = 0;
 	for (const descriptor in itemDescriptors) {
 		if (Object.hasOwn(itemDescriptors, descriptor)) {
 			const descriptorProperties: PropertyDescriptor = itemDescriptors[descriptor];
-			if (descriptorProperties.configurable) {
-				itemEntriesConfigurable.push(descriptor);
-			} else {
-				itemEntriesNonConfigurable.push(descriptor);
+			if (
+				!descriptorProperties.configurable ||
+				!descriptorProperties.enumerable ||
+				!descriptorProperties.writable ||
+				typeof descriptorProperties.get !== "undefined" ||
+				typeof descriptorProperties.set !== "undefined"
+			) {
+				return false;
 			}
-			if (descriptorProperties.enumerable) {
-				itemEntriesEnumerable.push(descriptor);
-			} else {
-				itemEntriesNonEnumerable.push(descriptor);
-			}
-			if (typeof descriptorProperties.get !== "undefined") {
-				itemEntriesGetter.push(descriptor);
-			}
-			if (typeof descriptorProperties.set !== "undefined") {
-				itemEntriesSetter.push(descriptor);
-			}
-			if (typeof descriptorProperties.get === "undefined" && typeof descriptorProperties.set === "undefined") {
-				itemEntriesNonAccessor.push(descriptor);
-			}
-			if (descriptorProperties.writable) {
-				itemEntriesWritable.push(descriptor);
-			} else {
-				itemEntriesNonWritable.push(descriptor);
-			}
+			enumerableCount += 1;
 		} else {
 			return false;
 		}
 	}
-	if (
-		Object.entries(item).length !== itemEntriesEnumerable.length ||
-		itemEntriesConfigurable.length + itemEntriesNonConfigurable.length !== itemEntriesEnumerable.length + itemEntriesNonEnumerable.length ||
-		itemEntriesEnumerable.length + itemEntriesNonEnumerable.length !== itemEntriesGetter.length + itemEntriesNonAccessor.length + itemEntriesSetter.length ||
-		itemEntriesGetter.length + itemEntriesNonAccessor.length + itemEntriesSetter.length !== itemEntriesNonWritable.length + itemEntriesWritable.length ||
-		itemEntriesConfigurable.length + itemEntriesNonConfigurable.length !== itemEntriesNonWritable.length + itemEntriesWritable.length ||
-		itemEntriesNonConfigurable.length > 0 ||
-		itemEntriesNonEnumerable.length > 0 ||
-		itemEntriesGetter.length > 0 ||
-		itemEntriesSetter.length > 0 ||
-		itemEntriesNonWritable.length > 0
-	) {
+	if (Object.entries(item).length !== enumerableCount) {
 		return false;
 	}
 	return true;
