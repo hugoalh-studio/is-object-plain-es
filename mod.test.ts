8a2194769ffbbce29ab3b09c760c321f1e46ba91
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-05-07 17:14:16 +0800

    Update resources

--- mod.test.ts
+++ mod.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "STD/assert/assert_equals.ts";
+import { assertEquals } from "STD/assert/assert-equals";
 import { isObjectPlain } from "./mod.ts";
 class Sample1 {
 	x: unknown;
