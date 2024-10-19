37df6a7099188950857d5f048ead7b127853ccce
hugoalh <hugoalh@gmail.com>
2024-10-19 16:43:46 +0800

    Update resources

--- .dnt.ts
+++ .dnt.ts
@@ -8,11 +8,11 @@ await invokeDenoNodeJSTransformer({
 		"LICENSE.md",
 		"README.md"
 	],
-	entrypoints: configJSR.exports,
+	entrypoints: configJSR.getExports(),
 	generateDeclarationMap: true,
 	metadata: {
-		name: "@hugoalh/is-object-plain",
-		version: configJSR.version,
+		name: configJSR.getName(),
+		version: configJSR.getVersion(),
 		description: "A module to determine whether the object is plain.",
 		keywords: [
 			"is",
